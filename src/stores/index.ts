c2b8496228af7438c162af0469dbc7e88c91783c
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-02-12 18:52:37 +0100

    reduxbaglanti

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import { UserState } from "../types/user";
+import userReducer from "./reducers/userReducer";
 
 //2 burada global typde girmek giyel olacak
 interface Appstate {
-    user:any;
+    user:UserState;
     categories:any;
     records:any;
 }
@@ -13,7 +15,7 @@ interface Appstate {
 //burada reducer state beliretcegziz
 //burada baslangictaki temel olarak state koyduk
 const rootReducer = combineReducers <Appstate>({
-     user:()=>{},
+     user:userReducer,
      categories:()=>{},
      records:()=>{}
 
--- src/stores/reducers/userReducer.ts
+++ src/stores/reducers/userReducer.ts
@@ -1,10 +1,22 @@
-import { UserAction, UserState } from "../../types/user";
+import {User, UserAction, UserState } from "../../types/user";
 
 
-//state kullanici bilgileri basta hat averir
+//2 bu asamda bir tane deafult state ihtiyacimiz var
+//datayi bos biareksak hat aliriz bunu yerin as User yazariz
+//sonra store gidip index ts icindeki user user Reducer yap
+
+const defaultState :UserState ={
+    data: {} as User,
+    loading:false,
+    error:''
+
+}
+
+//1state kullanici bilgileri basta hat averir
 //action da istek yaparken succe error start bunlaley types icindeki user yaprik 
-const userReducer = (state:UserState,action:UserAction)=>{
+const userReducer = (state:UserState=defaultState,action:UserAction)=>{
     return state;
 }
+
 
 export default userReducer;
