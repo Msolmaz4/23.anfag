78130afbb45442083f3592295daaaed527e4a714
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-02-12 18:39:40 +0100

    actiontemel

--- src/stores/reducers/userReducer.ts
+++ src/stores/reducers/userReducer.ts
@@ -1,8 +1,9 @@
-import { UserState } from "../../types/user";
+import { UserAction, UserState } from "../../types/user";
 
 
 //state kullanici bilgileri basta hat averir
-const userReducer = (state:UserState,action)=>{
+//action da istek yaparken succe error start bunlaley types icindeki user yaprik 
+const userReducer = (state:UserState,action:UserAction)=>{
     return state;
 }
 
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,13 +1,32 @@
+
+
 export interface User {
-    message:string,
-    username:string,
-    email:string,
-    full_name:string,
-    token:string
+    message:string;
+    username:string;
+    email:string;
+    full_name:string;
+    token:string;
 }
 
 export interface UserState {
-    data:User,
-    loading:boolean,
-    error:string
+    data:User;
+    loading:boolean;
+    error:string;
+
+
 }
+//LOGIN OLURKEN dispbac tpe olusturmak
+interface LOGIN_START {
+
+     type:'LOGIN_START';
+}
+//basarli olursa patload user gondermek istiyorum
+interface LOGIN_SUCCES {
+    type:'LOGIN_SUCCES';
+    payload:User;
+}
+interface LOGIN_ERROR{
+  type:'LOGIN_ERROR';
+}
+//BUNLARI ARADA TUTAN AXCTION YAPARIZ
+export type UserAction = LOGIN_START | LOGIN_SUCCES| LOGIN_ERROR
