be09844b64e9acb1645d0d73cea8946ac9d81194
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-02-12 14:08:22 +0100

    interafce

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,13 +1,22 @@
 import { combineReducers } from "redux";
 
+//2 burada global typde girmek giyel olacak
+interface Appstate {
+    user:any;
+    categories:any;
+    records:any;
+}
+
+
 
 //combine derudcer obje aliyor
 //burada reducer state beliretcegziz
 //burada baslangictaki temel olarak state koyduk
-const rootReducer = combineReducers({
+const rootReducer = combineReducers <Appstate>({
      user:()=>{},
-     category:()=>{},
+     categories:()=>{},
      records:()=>{}
 
 
-})
+}) 
+export default rootReducer;
