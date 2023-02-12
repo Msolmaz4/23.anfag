f0e47919b546e58aac7ce86f95047f9b14623803
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-02-12 14:01:36 +0100

    deduxtemel

---
+++ src/stores/index.ts
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+
+
+//combine derudcer obje aliyor
+//burada reducer state beliretcegziz
+//burada baslangictaki temel olarak state koyduk
+const rootReducer = combineReducers({
+     user:()=>{},
+     category:()=>{},
+     records:()=>{}
+
+
+})
